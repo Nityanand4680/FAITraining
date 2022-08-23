38ce5779fdf8912717456d5264d1e13473e6c400
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-08-23 17:04:54 +0530

    Add files via upload

---
+++ Ex07-ClassesAndObjects.cs
@@ -0,0 +1,39 @@
+﻿using System;
+/*
+ * A Class is a User defined Type(UDT) that can contain data members as well as functions.
+ * Classes allow U to create Composite types that can hold data of different data types representable as one Unit. They are used to represent real world objects. 
+ * A Class will contain a Defn and an Instance will be created for that class. The instance of the class is called as OBJECT. In other words, an object is a variable of the class.  
+ * A Class can have fields to represent data, methods to manipulate fields or data, properties as accessors to data. U can also have events(Actions that can be performed on the data). 
+ * From C# 4, U can also have nested classes. 
+ * With classes, comes a host of OOP features like Inheritance, Polymorphism, Encapsulation as well as Abstraction. 
+ * There can be different kinds of classes based on how U want to use them.
+ * Entity classes represent real world entities for the Application
+ * Data Classes represent the data of the Application which includes persistance(SAVING). 
+ * UI Classes that will interact with User and represent the User interface of the Application. 
+ * Utility Classes that are used for logging, General Messaging that are used across the Application. 
+ * Finally all classes are expected to represent an User defined type which are used using Objects. 
+ */
+namespace SampleConApp
+{
+    class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Email;
+        public string Address;
+    }
+
+    class ClassesAndObjects
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Address = "Bangalore";
+            emp.Name = "Phaniraj";
+            emp.Email = "phanirajbn@gmail.com";
+            emp.Id = 123;
+
+            Console.WriteLine($"The name is {emp.Name} from {emp.Address} and has an email Address as {emp.Email}");
+        }
+    }
+}
---
+++ Ex08-PropertiesDemo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+/*
+ * Properties are data accessors with get/set blocks for the fields.
+ * Fields are usually private to the class. 
+ * To access the private fields we use properties with get block or set block, sometimes both. 
+ */
+
+
+namespace SampleConApp
+{
+    class Book
+    {
+        int _id;//default is private in C#...
+        string _title;
+        string _author;
+        double _price;
+
+        public int BookID
+        {
+            get { return _id; }
+            set 
+            { 
+                if(value <= 0)
+                    throw new Exception("ID cannot be less than or equal to 0");
+                _id = value; 
+            }
+        }
+        //New in C#, we can use Lambda Operator => 
+        public string Title { get => _title; set => _title = value; }
+        public string Author { get => _author; set => _author = value; }
+        public double Price { get => _price; set => _price = value; }
+    }
+    class PropertiesDemo
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();//create the instance of the class.
+            book.BookID = 123;//set the values for the properties
+            book.Title = "Professional C#";
+            book.Author = "James Anderson";
+            book.Price = 650;
+
+            //Object initialization Syntax of C# 3.5
+            Book newBook = new Book { BookID = 123, Price = 567, Author ="Chetan Bhagat", Title="2 States" };
+            //With this syntax, U can set the properties in a single line. 
+
+            //Read and display the properties of the book in the console.
+            Console.WriteLine($"The Title of the Book is {book.Title} written by {book.Author} that costs {book.Price} and available at ISBN {book.BookID}");
+
+            Console.WriteLine($"The Title of the Book is {newBook.Title} written by {newBook.Author} that costs {newBook.Price} and available at ISBN {newBook.BookID}");
+        }
+
+        static void displayBookDetails()
+        {
+            Book newBook = new Book { BookID = 123, Price = 567, Author = "Chetan Bhagat", Title = "2 States" };
+            //Take input from the user on the title.
+            Console.WriteLine("Enter the title of the book to search");
+            string searchValue = Console.ReadLine();
+
+            //check for the title with our book....
+            if(newBook.Title == searchValue)
+            {
+                Console.WriteLine($"The Title of the Book is {newBook.Title} written by {newBook.Author} that costs {newBook.Price} and available at ISBN {newBook.BookID}");
+            }//else tell them that the book is not correct
+            else
+            {
+                Console.WriteLine("Book not found!!!");
+            }
+        }
+    }
+}
