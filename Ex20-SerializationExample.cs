787c9187d18e37618703ff173c984390205dcc4f
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-08-26 16:25:38 +0530

    Add files via upload

--- Ex20-SerializationExample.cs
+++ Ex20-SerializationExample.cs
@@ -5,13 +5,18 @@
  * Binary Serialization, XML Serialization and SOAP Serialization. 
  * Binary Serialization is used within the Windows OS. XML is used for Cross Operating System. SOAP is used for Web services.
  * For any serialization U need three things: Place of storage, Format of Storage and Data to store. 
+ * For Binary Serialization, the Class must be having an attribute called [Serializable]. This attribute is set for the class whose objects are to be serialized. 
+ * For XML serialization, U should make the class as public. 
  */
-using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using System; //Default namespace
+using System.Collections.Generic;//For Generic collection classes
+using System.IO;//For File IO classes
+using System.Runtime.Serialization.Formatters.Binary; //For BinaryFormatter class
+using System.Xml.Serialization;//For XmlSerializer
+
 namespace SampleConApp
 {
-    class SerializationDemo
+    class Ex20_SerializationDemo
     {
         static void BinarySave(Employee emp)//Employee class comes from Ex19 file..
         {
@@ -54,10 +59,37 @@ namespace SampleConApp
 
             return emp;
         }
+
+        static void saveAsXml(List<Employee> empList)
+        {
+            var fs = new FileStream("Data.xml", FileMode.OpenOrCreate, FileAccess.Write);
+            var formatter = new XmlSerializer(typeof(List<Employee>));//Should privide the Type Info of the class that U R serializing. 
+            formatter.Serialize(fs, empList);
+            fs.Close();
+        }
+
+        static List<Employee> loadXmlData()
+        {
+            var fs = new FileStream("Data.xml", FileMode.Open, FileAccess.Read);
+            var formatter = new XmlSerializer(typeof(List<Employee>));//Should privide the Type Info of the class that U R serializing. 
+            var list = formatter.Deserialize(fs) as List<Employee>;
+            fs.Close();
+            return list;
+        }
+
         static void Main(string[] args)
         {
             //BinarySave(createEmployee());
-            ReadData();
+            //ReadData();
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee { EmpName = "Ramesh", EmpAddress = "Mysore", EmpSalary = 56000 });
+            empList.Add(new Employee { EmpName = "Aravind", EmpAddress = "Tumkur", EmpSalary = 46000 });
+            empList.Add(new Employee { EmpName = "Suresh", EmpAddress = "Dharwad", EmpSalary = 56000 });
+            empList.Add(new Employee { EmpName = "Gopal", EmpAddress = "Chennai", EmpSalary = 45000 });
+            saveAsXml(empList);//For saving
+            
+            empList = loadXmlData();
+            foreach (var emp in empList) Console.WriteLine(emp.EmpName);
         }
     }
 }
