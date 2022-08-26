08fe84783b57d1cc4a7acdc912faf181be6030e9
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-08-26 12:37:14 +0530

    Add files via upload

--- Ex19-FileIODemo.cs
+++ Ex19-FileIODemo.cs
@@ -29,7 +29,7 @@ namespace SampleConApp
         public int EmpSalary { get; set; }
         public override string ToString()
         {
-            return $"{EmpName} with ID {EmpID} from {EmpAddress} earns a salary of Rs. {EmpSalary}";
+            return $"{EmpID}, {EmpName}, {EmpAddress}, {EmpSalary}";
         }
     }
     class FileIODemo
@@ -39,28 +39,76 @@ namespace SampleConApp
         {
             FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
             StreamWriter writer = new StreamWriter(fs);
-            writer.WriteLine($"{emp.EmpID},{emp.EmpName},{emp.EmpAddress},{emp.EmpSalary}");
+            writer.WriteLine(emp);//As the Class has overriden the ToString method, it writes in the format defined in the ToString method. 
             writer.Close();
             fs.Close();
         }
 
         static Employee readFromFile()
         {
-            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            StreamReader reader = new StreamReader(fs);
-            string data = reader.ReadLine();
-            string[] details = data.Split(',');
-            Employee emp = new Employee(int.Parse(details[0]));
-            emp.EmpName = details[1];
-            emp.EmpAddress = details[2];
-            emp.EmpSalary = int.Parse(details[3]);
-            fs.Close();
-            return emp;
+            if (File.Exists(fileName))
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(fs);
+                string data = reader.ReadLine();
+                string[] details = data.Split(',');
+                Employee emp = new Employee(int.Parse(details[0]));
+                emp.EmpName = details[1];
+                emp.EmpAddress = details[2];
+                emp.EmpSalary = int.Parse(details[3]);
+                fs.Close();
+                return emp;
+            }
+            throw new Exception("File does not exist");
+        }
+
+        static void writeUsingFile(Employee emp)
+        {
+            if (File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, emp.ToString());
+            }
+        }
+
+        static Employee readUsingFile()
+        {
+            if (File.Exists(fileName))
+            {
+                var lines = File.ReadLines(fileName);
+                foreach (var line in lines)
+                {
+                    string[] details = line.Split(',');
+                    Employee emp = new Employee(int.Parse(details[0]));
+                    emp.EmpName = details[1];
+                    emp.EmpAddress = details[2];
+                    emp.EmpSalary = int.Parse(details[3]);
+                    return emp;
+                }
+            }
+            throw new Exception("File not found to read");
         }
         static void Main(string[] args)
         {
-            //            writeToFile(new Employee { EmpName = "Phaniraj", EmpAddress = "Bangalore", EmpSalary = 67000 });
-            Console.WriteLine(readFromFile());
+            //writeToFile(new Employee { EmpName = "Phaniraj", EmpAddress = "Bangalore", EmpSalary = 67000 });
+            //Console.WriteLine(readFromFile());
+            //writeUsingFile(new Employee { EmpName = "Sunder", EmpAddress = "Mysore", EmpSalary = 77000 });
+            //Console.WriteLine(readUsingFile());
+            Employee emp = new Employee { EmpName = "Phaniraj", EmpAddress = "Bangalore", EmpSalary = 67000 };
+            writeFileToDirectory("C:\\SubDir", $"{emp.EmpName}.txt", emp);
+        }
+
+        private static void writeFileToDirectory(string dirName, string fileName, Employee details)
+        {
+            DirectoryInfo info;
+            if (Directory.Exists(dirName))
+            {
+                info = new DirectoryInfo(dirName);
+            }
+            else
+                info = Directory.CreateDirectory(dirName);
+            StreamWriter writer = File.CreateText(Path.Combine(info.FullName, fileName));
+            writer.WriteLine(details);
+            writer.Close();
         }
     }
 }
