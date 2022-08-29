910ddc8bc48bb429817a9040fb7a0e95005d6149
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-08-29 16:28:21 +0530

    Add files via upload

---
+++ Ex21-DelegatesExample.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * Delegates are functional References that can be used to pass function as an argument to a function. 
+ * Realtime, we use them to create Callback functions, Event Handlers, Search Criteria Functions, Multi Threading Apps and many more. 
+ * Normally functions will create parameters in the form of variables of a data type. In some scenarios, U would pass a variable(Reference) of a FUNCTION. The declaration of a Function signature is what is called as Delegate
+ * All Delegates are reference types.
+ * The Function that is passed as arg for the delegate instance must have the same signature of a the delegate. 
+ * .NET Gives 2 Generic Delegates called Func<T> and Action<T> where Func is used for Non-void functions and Action is used for void Functions. 
+ * Some of the popular .NET Provided Delegtes: Action, Func(Generic Delegates), Predicate(Finding in Collections), ThreadStart(Thread based), EventHandler(Windows and Web Event Handling)
+ * U can create delegate instances that can point to multiple functions at a time using Multicast Delegate. 
+*/
+
+using System;
+
+namespace SampleConApp
+{
+    delegate void FuncReference();
+
+    class Demo
+    {
+        public static void CallFunc(FuncReference func)
+        {
+            //Some work
+            Console.WriteLine("This function is called by the User of the Function");
+            func();         
+        }
+
+        //Use the built in delegate of .NET called Action. This method will be a void method.
+        public static void CallFuncWithAction(Action method)
+        {
+            Console.WriteLine("Action method is called");
+            if (method != null) method();
+        }
+        public static void CallActionFuncWithArgs(Action<string, int> func)
+        {
+            Console.WriteLine("Action method with args of string and int is called");
+            string firstParameter = Util.GetString("Enter the string for first Param");
+            int secondParameter = Util.GetNumber("Enter the int for second Param");
+            func(firstParameter, secondParameter);
+        }
+
+        public static void CallFuncWithArgs(Func<double, double, double> func)
+        {
+            var fValue = Util.GetDoubleNumber("Enter the first value");
+            var sValue = Util.GetDoubleNumber("Enter the second value");
+            var res = func(fValue, sValue);
+            Console.WriteLine("The result: " + res);
+        }
+    }
+    
+    class DelegateExample
+    {
+        static double addFunc(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+        static void TestFunc() => System.Console.WriteLine("Test Func");
+
+        static void AnotherFunc(int arg) => System.Console.WriteLine("Anothe Func with arg " + arg);
+
+        static void ActionFunc(string arg1, int arg2)
+        {
+            for (int i = 0; i < arg2; i++)
+            {
+                Console.WriteLine(arg1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //////////////Trying various kinds of Delegates: Custom, Action, Action<T> and Func<T1, T2, T3>
+            //Demo.CallFunc(TestFunc);
+            //Demo.CallFuncWithAction(TestFunc);
+
+            //Action<string, int> funcName = new Action<string, int>(ActionFunc);
+            //Demo.CallActionFuncWithArgs(funcName);//Old syntax
+
+            //Demo.CallActionFuncWithArgs(ActionFunc);//New syntax
+
+            //Demo.CallFuncWithArgs(addFunc);//For Action<double, double, double>
+
+            ///////////////////Using Anonymous method syntax in place of creating Explicit Fns/////////////
+            //Demo.CallFuncWithArgs(delegate (double v1, double v2)
+            //{
+            //    return v1 - v2 + (v1 * v2) / v2;
+            //});
+
+            /////////////////////Using Lambda Expressions in place of Anonymous methods/////////////
+            //Demo.CallFunc(() => Console.WriteLine("CallFunc is calling my Func"));
+            //Demo.CallFuncWithAction(() => Console.WriteLine("Action Method"));
+            //Demo.CallActionFuncWithArgs((str, i)=> Console.WriteLine("str: " + str ));
+
+            //Demo.CallFuncWithArgs( ( v1,  v2)  => v1 * v2);
+            //////////////////////////MultiCast Delegate///////////////////////////////////////////
+            Action delInstance = () => Console.WriteLine("Test Code 1st Func");
+            delInstance += () => Console.WriteLine("Another Code 2nd Func");
+            delInstance += () => Console.WriteLine("Another Code 3rd Func");
+            delInstance += () => Console.WriteLine("Another Code 4th Func");
+            
+            delInstance();
+
+            Func<int> func = () => 123;
+            func += () => 456;
+            func += () => 654;
+            func += () => 321;
+            int result = func();
+            Console.WriteLine(result + " is the value which will contain the result of the last method of the list");
+            //To get the result of each function in the multicast delegate:
+            var methods = func.GetInvocationList();//GetInvocationList will work for Multicast Delagates only. 
+            foreach(var instance in methods)
+            {
+                int res = (int)instance.DynamicInvoke();
+                Console.WriteLine(res + " from " + instance.Method.Name);
+            }
+
+        }
+    }
+}
---
+++ Ex22-MemoryManagementDemo.cs
@@ -0,0 +1,104 @@
+﻿/*
+ * .NET has its own automatic memory management handled by CLR. 
+ * All Code written in .NET Framework will be handled by the CLR. The Developer has no control on object deletion. 
+ * Developers create objects using new operator but do not have any API to explicitly delete it. 
+ * Desctructor will be called implicitly by the CLR when the object is destroyed. U cannot explicitly destroy an object or call destructor in UR Code. It is more like a clean up operation. Internally Destructor is converted to a Finalizer function by the CLR which means that this code is the final execution of the object. 
+ * U can ask the GC to collect the unused objects by calling an API GC.Collect. Anyways, U cannot specify the object name to be destroyed in it. 
+ * GC cleans up the unused objects by creating creating a seperate thread and does that parallely. 
+ * When U call the APIs of Unmanaged code(C++ DLLs,COM DLLs) in .NET U may have to destroy the Unmanaged objects explicitly. For that, UR Class can implement an interface called IDisposable and implement Dispose method where U can explicitly destroy the Unmanaged resources. 
+ * Caller of the objects created from the classes implementing IDisposable must call the Dispose method once the work with the unmanaged objects are completed. 
+ */
+
+
+using System;
+using System.Threading;
+
+namespace SampleConApp
+{
+    class Radio
+    {
+        private string _name;
+        public Radio(string name)
+        {
+            _name = name;
+        }
+        public void PlayMusic()
+        {
+            Console.WriteLine("Music is playing in " + _name);
+        }
+    }
+    class Car
+    {
+        public Radio myEntertainment { get; set; }
+        public Car()
+        {
+            myEntertainment = new Radio("SONY");
+        }
+
+        public Car(string radioName)
+        {
+            myEntertainment = new Radio(radioName);
+        }
+        public void Play()
+        {
+            Console.WriteLine("The Car now starts the Entertainment Device");
+            myEntertainment.PlayMusic();
+        }
+    }
+    class Data : IDisposable
+    {
+        private int arg = 132;
+        //Its a function used to construct an object.  U can use this function to inject the dependencies for UR Class.
+        public Data(int arg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            this.arg = arg;//this operator is used to resolve the local variable and field of the class. 
+            Console.WriteLine($"The object with Id {arg} is created");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        ~Data()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"The object with Id {arg} is destroyed");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public void Dispose()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"The object with Id {arg} is destroyed");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+    class GarbageCollectionDemo
+    {
+        static void createAndDestroyObjects()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                using(Data data  = new Data(i))//By using the using block, the Runtime will call the object's Dispose method internally if it exists. 
+                {
+                    Thread.Sleep(1000);
+                    GC.SuppressFinalize(data);
+                }
+                //Data data = new Data(i);
+                //data.Dispose();
+                //GC.SuppressFinalize(data);//Ensures that the Destructor of the object will not be called when it is deleted. 
+                //GC.Collect();//Forces the GC to clean up the memory asynchronously. 
+                //GC.WaitForPendingFinalizers();
+            }
+            Console.WriteLine("All object creations are completed");
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Which radio U want?");
+            //string type = Console.ReadLine();
+            //Car car = new Car(type);//Constructor Injection
+            ////car.myEntertainment = new Radio("JBL"); //Property injection
+            //car.Play();
+            createAndDestroyObjects();
+            Console.WriteLine("[MAIN] has ENDED!!!!!Time to terminate the App");
+        }
+    }
+}
