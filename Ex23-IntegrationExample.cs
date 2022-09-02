fca6223d206ed428a82b0bfdad192e93094e72b2
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-09-02 16:41:10 +0530

    Add files via upload

---
+++ Ex23-IntegrationExample.cs
@@ -0,0 +1,129 @@
+﻿using SampleConApp.DataLayer;
+using SampleConApp.MiddleLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace SampleConApp
+{
+    namespace UILayer
+    {
+        class MainClass
+        {
+            static IEmpBusinessComponent component = BusinessFactory.GetComponent();
+
+            static void Main(string[] args)
+            {
+                if (component == null) Console.WriteLine("No COMPONENT IS INTEGRATED INTO THE APP!!!");
+            }
+        }
+    }
+
+    namespace MiddleLayer
+    {
+        static class BusinessFactory
+        {
+            public static IEmpBusinessComponent GetComponent() => new MySimpleEmpBusComponent(new FileDataComponent());
+        }
+        class Employee
+        {
+            public int EmpID { get; set; }
+            public string EmpName { get; set; }
+            public string EmpAddress { get; set; }
+            public double EmpSalary { get; set; }
+        }
+
+        interface IEmpBusinessComponent
+        {
+            void AddNewEmployee(Employee emp);
+            void UpdateEmployee(Employee emp);
+
+            void DeleteEmployee(int id);
+
+            List<Employee> GetAllEmployees();
+        }
+
+        class MySimpleEmpBusComponent : IEmpBusinessComponent
+        {
+            private IDataLayer _component;
+
+            public MySimpleEmpBusComponent(IDataLayer component)
+            {
+                if (component == null) throw new Exception("Dal Component is not yet created");
+                _component = component;
+            }
+
+            public void AddNewEmployee(Employee emp)
+            {
+                //Check the emp object for allthe business rules. 
+                _component.AddNewEmployee(emp.EmpName, emp.EmpAddress, emp.EmpSalary);
+            }
+
+            public void DeleteEmployee(int id)
+            {
+                _component.DeleteEmployee(id);
+            }
+
+            public List<Employee> GetAllEmployees()
+            {
+                var list = new List<Employee>();
+                var data = _component.GetAllEmployees();
+                //Convert the row of the table to a Employee object and added to the list. 
+                foreach(DataRow row in data.Rows)
+                {
+                    var emp = new Employee
+                    {
+                        EmpID = Convert.ToInt32(row[0]),
+                        EmpName = row[1].ToString(),
+                        EmpAddress = row[2].ToString(),
+                        EmpSalary = Convert.ToDouble(row[3])
+                    };
+                    list.Add(emp);
+                }
+                return list;
+            }
+
+            public void UpdateEmployee(Employee emp)
+            {
+                _component.UpdateEmployee(emp.EmpID, emp.EmpName, emp.EmpAddress, emp.EmpSalary);
+            }
+        }
+    }
+
+    //Stores the data in a raw format:
+    namespace DataLayer
+    {
+        using System.Data;
+        interface IDataLayer
+        {
+            int AddNewEmployee(string name, string address, double salary);//Returns the id of the added emp
+            void UpdateEmployee(int id, string name, string address, double salary);
+            void DeleteEmployee(int id);
+
+            DataTable GetAllEmployees();
+        }
+
+        class FileDataComponent : IDataLayer
+        {
+            public int AddNewEmployee(string name, string address, double salary)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void DeleteEmployee(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public DataTable GetAllEmployees()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void UpdateEmployee(int id, string name, string address, double salary)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Ex24-ComparingObjects.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleConApp
+{
+    class Product : IComparable<Product>
+    {
+        static int no = 1000;
+        public Product()
+        {
+            ProductId = ++no;
+        }
+        public int ProductId { get; private set; }
+        public string ProductName { get; set; }
+        public int ProductCost { get; set; }
+        public string  Category { get; set; }
+
+        //Default sort is by ProductCost...
+        public int CompareTo(Product other)
+        {
+            if (ProductCost < other.ProductCost)
+                return -1;
+            else if (ProductCost > other.ProductCost)
+                return 1;
+            else
+                return 0;
+        }
+        public override string ToString()
+        {
+            return $"Name: {ProductName}\tPrice: {ProductCost}";
+        }
+    }
+
+    static class ProductRepo
+    {
+        private static List<Product> getAll()
+        {
+            var list = new List<Product>();
+            list.Add(new Product { ProductName = "Tennis Racquet", Category = "Sports", ProductCost = 560 });
+            list.Add(new Product { ProductName = "Tennis Ball", Category = "Sports", ProductCost = 60 });
+            list.Add(new Product { ProductName = "Smart Office Chair", Category = "Furniture", ProductCost = 4500 });
+            list.Add(new Product { ProductName = "Laptop Table", Category = "Furniture", ProductCost = 1560 });
+            list.Add(new Product { ProductName = "USB Pen Drive 16GB", Category = "Electronics", ProductCost = 800 });
+            list.Add(new Product { ProductName = "Ear Phone", Category = "Electronics", ProductCost = 2000 });
+            return list;
+        }
+        public static List<Product> AllProducts
+        {
+            get
+            {
+                return getAll();
+            }
+        }
+    }
+
+    enum ProductCriteria {  ByName, ByCategory, ById };
+    class ProductComparer : IComparer<Product>
+    {
+        private ProductCriteria _criteria;
+        public ProductComparer(ProductCriteria criteria)
+        {
+            _criteria = criteria;
+        }
+        public int Compare(Product x, Product y)
+        {
+            switch (_criteria)
+            {
+                case ProductCriteria.ByName:
+                    return x.ProductName.CompareTo(y.ProductName);
+                    
+                case ProductCriteria.ByCategory:
+                    return x.Category.CompareTo(y.Category);
+                   
+                case ProductCriteria.ById:
+                    return x.ProductId.CompareTo(y.ProductId);
+                default:
+                    return x.CompareTo(y);
+            }
+        }
+    }
+    class ComparingObjectsDemo
+    {
+        static void Main(string[] args)
+        {
+            var list = ProductRepo.AllProducts;
+            Console.WriteLine("Select any from the above list");
+            Array possibleValues = Enum.GetValues(typeof(ProductCriteria));
+            foreach (var value in possibleValues) Console.WriteLine(value);
+            ProductCriteria input = (ProductCriteria)Enum.Parse(typeof(ProductCriteria), Console.ReadLine());
+            list.Sort(new ProductComparer(input));//It will try to call any of the object's IComparable Function..
+
+            foreach (var item in list) Console.WriteLine(item);
+        }
+    }
+}
